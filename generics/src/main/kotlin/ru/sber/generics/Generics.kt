902515fe2b86624cd7f5f9776ef2cf6114750e5e
Ramil <ram.ru@mail.ru>
2021-09-20 19:22:32 +0300

    generics

--- generics/src/main/kotlin/ru/sber/generics/Generics.kt
+++ generics/src/main/kotlin/ru/sber/generics/Generics.kt
@@ -1,27 +1,42 @@
 package ru.sber.generics
 
-import com.sun.org.apache.xpath.internal.operations.Bool
-import java.util.*
 
 // 1.
-//fun compare(p1: Pair, p2: Pair): Boolean {
-//    return false
-//}
+fun <A, B> compare(p1: Pair<out A, B>, p2: Pair<out A, B>): Boolean {
+    val isEquality = if (p1 == p2) true else false
+    return isEquality
+}
 
 // 2.
-fun countGreaterThan(anArray: Array<Any>, elem: Any): Int {
-    return 0
+fun <Any : Comparable<Any>> countGreaterThan(anArray: Array<Any>, elem: Any): Int {
+    var sum = 0
+    anArray.filter { it > elem }.forEach { ++sum }
+    return sum
 }
 
 // 3.
-class Sorter {
-//    val list: MutableList
-//
-//    fun add(value: Any) {
-//    }
+class Sorter<T : Comparable<T>> {
+    val list: MutableList<T> = mutableListOf()
+
+    fun add(value: T) {
+        list.add(value)
+        list.sortWith(compareBy { it })
+    }
 }
 
 // 4.
-class Stack {
+class Stack<T> {
+    val stack: MutableList<T> = mutableListOf()
 
+    fun push(s: T) {
+        stack.add(s)
+    }
+
+    fun pop(): T {
+        return stack.removeLast()
+    }
+
+    fun isEmpty(): Boolean {
+        return stack.isEmpty()
+    }
 }
